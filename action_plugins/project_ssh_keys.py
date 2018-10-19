5fc5e137fea3989b3caec70c86cbbf95afc65752
Jay Cheroske <jay@portal.org>
2018-10-19 10:22:11 -0700

    project ssh keys

--- action_plugins/project_ssh_keys.py
+++ action_plugins/project_ssh_keys.py
@@ -40,8 +40,6 @@ class ActionModule(ActionBase):
         # get the metadata fingerprint
         fingerprint = projects_api.get(project=project).execute()['commonInstanceMetadata']['fingerprint']
 
-        print("Fingerprint: " + fingerprint)
-
         # set the ssh-key metadata value
         request_body = {
             'fingerprint': fingerprint,
