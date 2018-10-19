1bf37a1af18c134b95adb2fe5a6273a1bb1371d0
Jay Cheroske <jay@portal.org>
2018-10-19 01:00:49 -0700

    project ssh keys

---
+++ action_plugins/project_ssh_keys.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+
+from ansible.errors import AnsibleError
+from ansible.plugins.action import ActionBase
+from ansible.utils.vars import merge_hash
+
+try:
+    from googleapiclient import discovery
+    from googleapiclient.errors import HttpError
+
+    HAS_API_CLIENT = True
+except ImportError:
+    HAS_API_CLIENT = False
+
+
+class ActionModule(ActionBase):
+    def run(self, tmp=None, task_vars=None):
+        if not HAS_API_CLIENT:
+            raise AnsibleError("Please install the google-api-python-client library")
+
+        # combine task and action vars, and call it context
+        if task_vars is None:
+            task_vars = dict()
+
+        action_vars = self._task.args
+        context = merge_hash(task_vars, action_vars)
+
+        # get variables
+        project = context['project']
+        keys = context['keys']
+
+        # get the projects api service object
+        projects_api = discovery.build('compute', 'v1').projects()
+
+        # get the metadata fingerprint
+        fingerprint = projects_api.get(project=project).commonInstanceMetadata.fingerprint
+
+        # set the ssh-key metadata value
+        request_body = {
+            'fingerprint': fingerprint,
+            'items': [{
+                'key': 'ssh-keys',
+                'value': "\n".join(keys),
+            }]
+        }
+        result = projects_api.setCommonInstanceMetadata(project=project, body=request_body)
+
+        # raise if the result contains errors
+        if len(result.get('error', {}).get('errors', [])):
+            raise AnsibleError('Error while setting project metadata', result['error'])
+
+        # get the return object from the parent class
+        return_value = super(ActionModule, self).run(tmp, task_vars)
+
+        # put the resources under the 'result' key
+        return_value['result'] = result
+
+        return return_value
+
